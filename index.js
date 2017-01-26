8d1f6a232f581bf50db1311cc1777ddbfc1d099d
Alex Peattie <alexpeattie@gmail.com>
2017-01-26 12:23:18 +0000

    Final implementation + chat client

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
 const _ = require('lodash')
 const uuid = require('uuid/v4')
 const request = require('request')
+const path = require('path')
 const express = require('express')
+const http = require('http')
 const bodyParser = require('body-parser')
+const WebSocket = require('ws')
+const process = require('process')
+const Q = require('q')
+
+// Config constants
+const numNodes = 3
+const refreshFrequencyMs = 200
+const baseUrl = 'http://localhost'
 
 class Node {
   constructor(endpoint) {
@@ -22,6 +32,10 @@ class Node {
     this.peers.push(peer)
   }
 
+  findOrCreatePeer(endpoint) {
+    return _.find(this.peers, { endpoint: endpoint }) || this.addPeer(new Node(endpoint))
+  }
+
   randomPeer() {
     return _.sample(this.peers)
   }
@@ -35,12 +49,13 @@ class Node {
     const messageId = [this.uuid, this.sequenceNum() + 1].join(':')
 
     this.messages[messageId] = {
-      messageId: messageId,
-      originator: details.originator,
-      text: details.text,
-      endpoint: this.endpoint
+      MessageID: messageId,
+      Originator: details.Originator,
+      Text: details.Text,
+      EndPoint: this.endpoint
     }
     this.seen[this.uuid] = this.sequenceNum()
+    return messageId
   }
 
   propogateMessage() {
@@ -48,13 +63,13 @@ class Node {
     if(!peer) return
     let message
 
-    if(Math.random() > 0) {
+    if(Math.random() > 0.5) {
       // pick a random origin with messages we haven't yet sent to the randomly selected peer
-      const messageOrigin = _.sample(this.originsWithUnsentMessages(peer))
+      const messageOrigin = _.sample(this.originsWithUnseenMessages(peer.seen))
       if(!messageOrigin) return // <- peer is up-to-date
-      // console.log([messageOrigin, (peer.seen[messageOrigin] || 0) + 1].join(':'))
+
       // let's send the peer the next message in the sequence, after the most recent message they saw
-      message = this.rumor([messageOrigin, (peer.seen[messageOrigin] || 0) + 1].join(':'))
+      message = this.nextMessage(messageOrigin, peer.seen)
     } else {
       message = this.want()
     }
@@ -62,12 +77,16 @@ class Node {
     this.send(message, peer)
   }
 
+  nextMessage(messageOrigin, seenMessages) {
+    return this.rumor([messageOrigin, (seenMessages[messageOrigin] || 0) + 1].join(':'))
+  }
+
   rumor(messageId) {
-    return { rumor: this.messages[messageId], endpoint: this.endpoint }
+    return { Rumor: this.messages[messageId], EndPoint: this.endpoint }
   }
 
   want(messageId) {
-    return { want: this.seen, endpoint: this.endpoint }
+    return { Want: this.seen, EndPoint: this.endpoint }
   }
 
   parseId(messageId) {
@@ -76,18 +95,18 @@ class Node {
   }
 
   send(message, peer) {
-    if(message.rumor) {
-      const [originId, sequenceNum] = this.parseId(message.rumor.messageId)
-      peer.seen[originId] = sequenceNum
-    }
-
-    request.post(peer.endpoint, { json: message })
+    request.post(peer.endpoint, { json: message }, (error, response) => {
+      if (!error && response.statusCode == 200 && message.Rumor) {
+        const [originId, sequenceNum] = this.parseId(message.Rumor.MessageID)
+        if(peer) peer.seen[originId] = sequenceNum
+      }
+    })
   }
 
-  originsWithUnsentMessages(peer) {
-    const untrackedNodes = _.difference(_.keys(this.seen), _.keys(peer.seen))
+  originsWithUnseenMessages(seenMessages) {
+    const untrackedNodes = _.difference(_.keys(this.seen), _.keys(seenMessages))
 
-    const nodesWithUnsentMessages = _.keys(_.pickBy(peer.seen, (messageNum, nodeId) => {
+    const nodesWithUnsentMessages = _.keys(_.pickBy(seenMessages, (messageNum, nodeId) => {
       return this.seen[nodeId] > messageNum
     }))
 
@@ -95,59 +114,116 @@ class Node {
   }
 
   receiveMessage(message) {
-    if(message.want) return
-    const messageContent = (message.rumor || message.want)
-    let [originId, sequenceNum] = this.parseId(messageContent.messageId)
-    sequenceNum = parseInt(sequenceNum, 10)
+    const messageContent = (message.Rumor || message.Want)
+    let peer = this.findOrCreatePeer(message.EndPoint)
 
-    if(this.seen[originId] > sequenceNum) return // we've already a newer message than this, we can ignore it
-    this.messages[messageContent.messageId] = messageContent
-    this.seen[originId] = sequenceNum
+    if(message.Want) {
+      this.originsWithUnseenMessages(message.Want).forEach(messageOrigin => {
+        this.send(this.nextMessage(messageOrigin, message.Want), peer)
+      })
+    } else if(message.Rumor) {
+      let [originId, sequenceNum] = this.parseId(messageContent.MessageID)
+      sequenceNum = parseInt(sequenceNum, 10)
+
+      if(this.seen[originId] > sequenceNum) return // we've already a newer message than this, we can ignore it
+      this.messages[messageContent.MessageID] = messageContent
+      this.seen[originId] = sequenceNum
+      if(messageContent.Text == 'secret') console.log(message, this.messages)
+      return messageContent
+    } else {
+      // something else, ignore...
+    }
   }
 }
 
+let socketServers = []
 let nodes = []
 _.range(0, 3).forEach(n => {
   let port = 3010 + n
-  let server = express()
-  server.use(bodyParser.json())
+  let app = express()
+  app.use(bodyParser.json())
 
-  server.listen(port, () => {
-    console.log('Example app listening on port ' + port)
+  nodes.push(new Node([baseUrl, port].join(':') + '/gossip'))
+
+  const server = http.createServer(app)
+  const wss = new WebSocket.Server({ server })
+  socketServers.push(wss)
+
+  app.post('/gossip', (req, res) => {
+    if(nodes[n].stopped) {
+      res.sendStatus(503)
+    } else {
+      let newMessage = nodes[n].receiveMessage(req.body)
+      if(newMessage) {
+        wss.clients.forEach(client => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ [newMessage.MessageID]: newMessage }))
+          }
+        })
+      }
+
+      res.sendStatus(200)
+    }
   })
 
-  nodes.push(new Node('http://localhost:' + port + '/receive'))
-  if(nodes[n - 1]) nodes[n].addPeer(nodes[n - 1])
+  app.post('/message', (req, res) => {
+    let messageId = nodes[n].newMessage(req.body)
 
-  server.post('/receive', function (req, res) {
-    nodes[n].receiveMessage(req.body)
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({ MessageID: messageId }))
+  })
+
+  app.post('/nodes', (req, res) => {
+    nodes[n].findOrCreatePeer(req.body.endpoint)
     res.sendStatus(200)
   })
 
-  setInterval(nodes[n].propogateMessage.bind(nodes[n]), 500)
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+  })
+  app.on('upgrade', wss.handleUpgrade);
+
+  wss.on('connection', ws => {
+    ws.send(JSON.stringify(nodes[n].messages))
+  })
+
+  server.listen(port, () => {
+    console.log(`Node ${ n + 1 } (Origin ID: ${ nodes[n].uuid }) listening on ${ nodes[n].endpoint.replace('/gossip', '') }`)
+  })
+  setInterval(nodes[n].propogateMessage.bind(nodes[n]), refreshFrequencyMs)
 })
 
-// console.log(nodes.length)
+nodes.forEach(node => {
+  _.without(nodes, node).forEach(partner => node.addPeer(partner))
+})
 
-// let nodeA = new Node('http://localhost:3010/receive')
-// let nodeB = new Node('http://localhost:3011/receive')
-nodes[2].newMessage({ text: 'a', originator: 'Alex' })
+setTimeout(() => { console.log("To stop a node, enter `stop n`. To start a stopped node, enter `start n`") }, 200)
 
+process.openStdin().addListener('data', d => {
+  const input = d.toString().trim()
+  let commandParse = input.match(/^(start|stop) ([0-9]+)/)
+  let match, command, nodeNum
+  
+  if(commandParse) {
+    [match, command, nodeNum] = commandParse
+  } else {
+    console.log(`Unrecognized command ${ input }`)
+    return
+  }
 
-setInterval(nodes[2].propogateMessage.bind(nodes[2]), 500)
+  nodeNum = parseInt(nodeNum, 10) - 1
+  nodes[nodeNum].stopped = (command == 'stop')
+  console.log(`Node ${ nodeNum + 1 } has been ${ command == 'stop' ? 'stopped' : 'started' }`)
+})
 
-// nodeA.newMessage({ text: 'b', originator: 'Fred' })
-// nodeA.addPeer(nodeB)
+process.once('SIGUSR2', function () {
+  const activeClients = _.flatten(socketServers.map(s => s.clients)).filter(client => client.readyState === WebSocket.OPEN)
 
-// setTimeout(nodeA.propogateMessage.bind(nodeA), 500)
-// setTimeout(nodeA.propogateMessage.bind(nodeA), 1500)
-
-// server.post('/receive', function (req, res) {
-//   nodeB.receiveMessage(req.body)
-//   console.log(nodeB)
-//   res.send("Hello");
-// })
-
-// server.listen(3011, function () {
-//   console.log('Example app listening on port 3011!')
-// })
+  Q.all(activeClients.map(client => {
+    let deferred = Q.defer()
+    client.send(JSON.stringify({ refresh: true }), {}, data => deferred.resolve(data))
+    return deferred
+  })).then(() => {
+    setTimeout(() => { process.kill(process.pid, 'SIGUSR2') }, 200)
+  })
+})
